19dca9b8fc2e9e051043992d01de4c6346793b7d
Lynna-wh <riski6587@gmail.com>
2024-08-24 12:04:59 +0800

    update table dashboard
    Memperbarui Tabel dan Dashboard Admin:
    
    1.Menghapus bagian Home/Daftar... pada masing-masing tabel.
    2. Mengubah judul tabel agar sesuai dengan masing-masing dashboard.
    3. Mengubah kolom tabel untuk menampilkan:
    -Nomor
    -Nama
    -Jenis lomba
    -Aksi
    4.Menambahkan tombol Update dan Delete di kolom aksi pada masing-masing tabel.
    5.Menghapus bagian <tfoot> untuk menyederhanakan tampilan tabel.

--- app/Views/daftar_rules.php
+++ app/Views/daftar_rules.php
@@ -1,178 +1,163 @@
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-      <!-- Content Header (Page header) -->
-      <section class="content-header">
-          <div class="container-fluid">
-              <div class="row mb-2">
-                  <div class="col-sm-6">
-                      <h1>Blank Page</h1>
-                  </div>
-                  <div class="col-sm-6">
-                      <ol class="breadcrumb float-sm-right">
-                          <li class="breadcrumb-item"><a href="#">Home</a></li>
-                          <li class="breadcrumb-item active">Blank Page</li>
-                      </ol>
-                  </div>
-              </div>
-          </div><!-- /.container-fluid -->
-      </section>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Daftar Rules</h1>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
 
-      <!-- Main content -->
-      <section class="content">
+    <!-- Main content -->
+    <section class="content">
 
-          <!-- Modal Add -->
-          <form>
-              <div class="modal fade" id="modal-lg">
-                  <div class="modal-dialog modal-lg">
-                      <div class="modal-content">
-                          <div class="modal-header">
-                              <h4 class="modal-title">Extra Large Modal</h4>
-                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                  <span aria-hidden="true">&times;</span>
-                              </button>
-                          </div>
-                          <div class="modal-body">
-                              <div class="card-body">
-                                  <div class="form-group">
-                                      <label for="exampleInputEmail1">Email address</label>
-                                      <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                                  </div>
-                                  <div class="form-group">
-                                      <label for="exampleInputPassword1">Password</label>
-                                      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                                  </div>
-                                  <div class="form-group">
-                                      <label for="exampleInputFile">File input</label>
-                                      <div class="input-group">
-                                          <div class="custom-file">
-                                              <input type="file" class="custom-file-input" id="exampleInputFile">
-                                              <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                                          </div>
-                                          <div class="input-group-append">
-                                              <span class="input-group-text">Upload</span>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                              <!-- /.card-body -->
+        <!-- Modal Add -->
+        <form>
+            <div class="modal fade" id="modal-lg">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title">Extra Large Modal</h4>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Email address</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1"
+                                        placeholder="Enter email">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1">Password</label>
+                                    <input type="password" class="form-control" id="exampleInputPassword1"
+                                        placeholder="Password">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputFile">File input</label>
+                                    <div class="input-group">
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input" id="exampleInputFile">
+                                            <label class="custom-file-label" for="exampleInputFile">Choose
+                                                file</label>
+                                        </div>
+                                        <div class="input-group-append">
+                                            <span class="input-group-text">Upload</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <div class="modal-footer justify-content-between">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                    <!-- /.modal-content -->
+                </div>
+                <!-- /.modal-dialog -->
+            </div>
+            <!-- /.modal -->
+        </form>
 
-                              <!-- /.card -->
-                          </div>
-                          <div class="modal-footer justify-content-between">
-                              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                              <button type="button" class="btn btn-primary">Save changes</button>
-                          </div>
-                      </div>
+        <!-- Modal Update -->
+        <form>
+            <div class="modal fade" id="modal-lg-update">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title">Extra Large Modal</h4>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="exampleInputEmail2">Email address</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail2"
+                                        placeholder="Enter email">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword2">Password</label>
+                                    <input type="password" class="form-control" id="exampleInputPassword2"
+                                        placeholder="Password">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputFile2">File input</label>
+                                    <div class="input-group">
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input" id="exampleInputFile2">
+                                            <label class="custom-file-label" for="exampleInputFile2">Choose
+                                                file</label>
+                                        </div>
+                                        <div class="input-group-append">
+                                            <span class="input-group-text">Upload</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <div class="modal-footer justify-content-between">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                    <!-- /.modal-content -->
+                </div>
+                <!-- /.modal-dialog -->
+            </div>
+            <!-- /.modal -->
+        </form>
 
-                      <!-- /.modal-content -->
-                  </div>
-                  <!-- /.modal-dialog -->
-              </div>
-              <!-- /.modal -->
-          </form>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-lg">
+            Launch Large Modal
+        </button>
 
-          <!-- Modal Update -->
-          <form>
-              <div class="modal fade" id="modal-lg">
-                  <div class="modal-dialog modal-lg">
-                      <div class="modal-content">
-                          <div class="modal-header">
-                              <h4 class="modal-title">Extra Large Modal</h4>
-                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                  <span aria-hidden="true">&times;</span>
-                              </button>
-                          </div>
-                          <div class="modal-body">
-                              <div class="card-body">
-                                  <div class="form-group">
-                                      <label for="exampleInputEmail1">Email address</label>
-                                      <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                                  </div>
-                                  <div class="form-group">
-                                      <label for="exampleInputPassword1">Password</label>
-                                      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                                  </div>
-                                  <div class="form-group">
-                                      <label for="exampleInputFile">File input</label>
-                                      <div class="input-group">
-                                          <div class="custom-file">
-                                              <input type="file" class="custom-file-input" id="exampleInputFile">
-                                              <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                                          </div>
-                                          <div class="input-group-append">
-                                              <span class="input-group-text">Upload</span>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                              <!-- /.card-body -->
+        <br> <br>
 
-                              <!-- /.card -->
-                          </div>
-                          <div class="modal-footer justify-content-between">
-                              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                              <button type="button" class="btn btn-primary">Save changes</button>
-                          </div>
-                      </div>
+        <!-- Default box -->
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Daftar Rules</h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+                <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th style="width: 10px">No</th>
+                            <th>Nama</th>
+                            <th>Jenis Lomba</th>
+                            <th style="width: 200px">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1.</td>
+                            <td>John Doe</td>
+                            <td>Fotografi</td>
+                            <td>
+                                <button type="button" class="btn btn-primary btn-sm" data-toggle="modal"
+                                    data-target="#modal-lg-update">Update</button>
+                                <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                            </td>
+                        </tr>
+                        <!-- Tambahkan lebih banyak baris sesuai kebutuhan -->
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
 
-                      <!-- /.modal-content -->
-                  </div>
-                  <!-- /.modal-dialog -->
-              </div>
-              <!-- /.modal -->
-          </form>
-
-          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-lg">
-              Launch Large Modal
-          </button>
-
-          <br> <br>
-
-          <!-- Default box -->
-          <div class="card">
-              <div class="card-header">
-                  <h3 class="card-title">Daftar Rules</h3>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                  <table id="example1" class="table table-bordered table-striped">
-                      <thead>
-                          <tr>
-                              <th>Rendering engine</th>
-                              <th>Browser</th>
-                              <th>Platform(s)</th>
-                              <th>Engine version</th>
-                              <th>CSS grade</th>
-                          </tr>
-                      </thead>
-                      <tbody>
-                          <tr>
-                              <td>Trident</td>
-                              <td>Internet
-                                  Explorer 4.0
-                              </td>
-                              <td>Win 95+</td>
-                              <td> 4</td>
-                              <td>X</td>
-                          </tr>
-                      </tbody>
-                      <tfoot>
-                          <tr>
-                              <th>Rendering engine</th>
-                              <th>Browser</th>
-                              <th>Platform(s)</th>
-                              <th>Engine version</th>
-                              <th>CSS grade</th>
-                          </tr>
-                      </tfoot>
-                  </table>
-              </div>
-              <!-- /.card-body -->
-          </div>
-          <!-- /.card -->
-          <!-- /.card -->
-
-      </section>
-      <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
--- app/Views/dashboard_admin.php
+++ app/Views/dashboard_admin.php
@@ -1,51 +1,69 @@
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-      <!-- Content Header (Page header) -->
-      <section class="content-header">
-          <div class="container-fluid">
-              <div class="row mb-2">
-                  <div class="col-sm-6">
-                      <h1>Blank Page</h1>
-                  </div>
-                  <div class="col-sm-6">
-                      <ol class="breadcrumb float-sm-right">
-                          <li class="breadcrumb-item"><a href="#">Home</a></li>
-                          <li class="breadcrumb-item active">Blank Page</li>
-                      </ol>
-                  </div>
-              </div>
-          </div><!-- /.container-fluid -->
-      </section>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Dashboard Admin</h1>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
 
-      <!-- Main content -->
-      <section class="content">
-
-          <!-- Default box -->
-          <div class="card">
-              <div class="card-header">
-                  <h3 class="card-title">Title</h3>
-
-                  <div class="card-tools">
-                      <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                          <i class="fas fa-minus"></i>
-                      </button>
-                      <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                          <i class="fas fa-times"></i>
-                      </button>
-                  </div>
-              </div>
-              <div class="card-body">
-                  Start creating your amazing application!
-              </div>
-              <!-- /.card-body -->
-              <div class="card-footer">
-                  Footer
-              </div>
-              <!-- /.card-footer-->
-          </div>
-          <!-- /.card -->
-
-      </section>
-      <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+    <!-- Main content -->
+    <section class="content">
+        <!-- Default box -->
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Daftar Rules</h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+                <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th style="width: 10px">No</th>
+                            <th>Nama</th>
+                            <th>Jenis Lomba</th>
+                            <th style="width: 100px">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>John Doe</td>
+                            <td>Fotografi</td>
+                            <td>
+                                <button type="button" class="btn btn-primary btn-sm">Update</button>
+                                <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Jane Doe</td>
+                            <td>Videografi</td>
+                            <td>
+                                <button type="button" class="btn btn-primary btn-sm">Update</button>
+                                <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                            </td>
+                        </tr>
+                        <!-- Tambahkan lebih banyak baris sesuai kebutuhan -->
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th style="width: 10px">No</th>
+                            <th>Nama</th>
+                            <th>Jenis Lomba</th>
+                            <th style="width: 200px">Aksi</th>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
