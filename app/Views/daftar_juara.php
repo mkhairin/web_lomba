0776fff3b72cc987f73fd13d1ce731fda446b0b9
Muhammad Khairin <88011174+mkhairin@users.noreply.github.com>
2024-08-29 11:52:03 +0800

    Update admin dashboard

--- app/Views/daftar_juara.php
+++ app/Views/daftar_juara.php
@@ -149,7 +149,7 @@
         <!-- Default box -->
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">Daftar Lomba</h3>
+                <h3 class="card-title">Daftar Juara</h3>
             </div>
             <!-- /.card-header -->
             <div class="card-body">
