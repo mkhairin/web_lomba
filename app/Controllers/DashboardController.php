409abefa13fadf39282c3b4c83a3cfb8ed0f8d10
Muhammad Khairin <88011174+mkhairin@users.noreply.github.com>
2024-09-01 23:26:53 +0800

    Fix conflict

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -1,13 +1,11 @@
 <?php
-
 namespace App\Controllers;
-
 class DashboardController extends BaseController
 {
     public function index()
     {
-        $header['title'] = 'Dashboard';
-        echo view('partial/header', $header);
+        $header['title']='Dashboard';
+        echo view('partial/header',$header);
         echo view('partial/top_menu');
         echo view('partial/side_menu');
         echo view('dashboard_admin');
@@ -16,8 +14,8 @@ class DashboardController extends BaseController
 
     public function daftarLomba()
     {
-        $header['title'] = 'Daftar Lomba';
-        echo view('partial/header', $header);
+        $header['title']='Daftar Lomba';
+        echo view('partial/header',$header);
         echo view('partial/top_menu');
         echo view('partial/side_menu');
         echo view('daftar_lomba');
@@ -26,8 +24,8 @@ class DashboardController extends BaseController
 
     public function daftarRules()
     {
-        $header['title'] = 'Daftar Rules';
-        echo view('partial/header', $header);
+        $header['title']='Daftar Rules';
+        echo view('partial/header',$header);
         echo view('partial/top_menu');
         echo view('partial/side_menu');
         echo view('daftar_rules');
@@ -36,31 +34,111 @@ class DashboardController extends BaseController
 
     public function daftarSponsor()
     {
-        $header['title'] = 'Daftar Sponsor';
-        echo view('partial/header', $header);
+        $header['title']='Daftar Sponsor';
+        echo view('partial/header',$header);
         echo view('partial/top_menu');
         echo view('partial/side_menu');
         echo view('daftar_sponsor');
         echo view('partial/footer');
     }
 
+    public function daftarJuara()
+    {
+        $header['title']='Daftar Juara';
+        echo view('partial/header',$header);
+        echo view('partial/top_menu');
+        echo view('partial/side_menu');
+        echo view('daftar_juara');
+        echo view('partial/footer');
+    }
+
     public function daftarPeserta()
     {
-        $header['title'] = 'Daftar Peserta';
-        echo view('partial/header', $header);
+        $header['title']='Daftar Peserta';
+        echo view('partial/header',$header);
         echo view('partial/top_menu');
         echo view('partial/side_menu');
         echo view('daftar_peserta');
         echo view('partial/footer');
     }
 
+    public function daftarPembimbing()
+    {
+        $header['title'] = 'Daftar Pembimbing';
+        echo view('partial/header', $header);
+        echo view('partial/top_menu');
+        echo view('partial/side_menu');
+        echo view('daftar_pembimbing');
+        echo view('partial/footer');
+    }
+
+
+
+
+
+
+    // sekolah methods
     public function daftarSekolah()
     {
-        $header['title']='Daftar Juara';
+        $Model = new \App\Models\SekolahModel();
+        $header['title']='Daftar Sekolah';
+
+        $data['dataSekolah'] = $Model->getdata();
+
         echo view('partial/header',$header);
         echo view('partial/top_menu');
         echo view('partial/side_menu');
-        echo view('daftar_sekolah');
+        echo view('daftar_sekolah', $data);
         echo view('partial/footer');
+    }
+
+    public function sekolahInsert(){
+        $Model = new \App\Models\SekolahModel();
+        $data = [
+            'id_sekolah' => $this->request->getPost("id"),
+            'nama_sekolah' => $this->request->getPost("nama_sekolah"),
+            'alamat' => $this->request->getPost("alamat"),
+        ];
+
+        if ($Model->insertData($data)) {
+            session()->setFlashdata('success', 'Data Berhasil Ditambah!');
+        } else {
+            session()->setFlashdata('error', 'Data Gagal Ditambah!');
+        }
+
+        return redirect()->to('/daftar-sekolah');
+    }
+
+    public function updateData($id)
+    {
+
+        $Model = new \App\Models\SekolahModel();
+        $data = [
+            'id_sekolah' => $this->request->getPost("id"),
+            'nama_sekolah' => $this->request->getPost("nama_sekolah"),
+            'alamat' => $this->request->getPost("alamat"),
+        ];
+
+        if ($Model->updateData($id, $data)) {
+            session()->setFlashdata('success', 'Data Berhasil Diubah!');
+        } else {
+            session()->setFlashdata('error', 'Data Gagal Diubah!');
+        }
+
+        return redirect()->to('/daftar-sekolah');
+    }
+
+    public function deleteData($id)
+    {
+
+        $Model = new \App\Models\SekolahModel();
+        if ($Model == true) {
+            $Model->deleteData($id);
+            session()->setFlashdata('success', 'Data Berhasil Dihapus!');
+        } else {
+            session()->setFlashdata('error', 'Data Gagal Dihapus!');
+        }
+
+        return redirect()->to('/daftar-sekolah');
     }
 }
--- app/Views/daftar_juara.php
+++ app/Views/daftar_juara.php
@@ -14,25 +14,13 @@
     <!-- Main content -->
     <section class="content">
 
-        <?php if (session()->getFlashdata('success')): ?>
-            <div class="alert alert-success">
-                <?= session()->getFlashdata('success'); ?>
-            </div>
-        <?php endif; ?>
-
-        <?php if (session()->getFlashdata('error')): ?>
-            <div class="alert alert-danger">
-                <?= session()->getFlashdata('error'); ?>
-            </div>
-        <?php endif; ?>
-
         <!-- Modal Add -->
-        <form action="/daftar-juara/insert" method="post">
+        <form>
             <div class="modal fade" id="modal-lg">
                 <div class="modal-dialog modal-lg">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h4 class="modal-title">Tambah Daftar Sponsor</h4>
+                            <h4 class="modal-title">Tambah Daftar Lomba</h4>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
@@ -41,18 +29,35 @@
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-md-6">
-                                        <input type="hidden" id="id" name="id">
                                         <div class="form-group">
-                                            <label for="juara">Juara</label>
-                                            <input type="text" class="form-control" id="juara" name="juara" placeholder="Masukkan kategori">
+                                            <label>Minimal</label>
+                                            <select class="form-control select2" style="width: 100%;">
+                                                <option selected="selected">Alabama</option>
+                                                <option>Alaska</option>
+                                                <option>California</option>
+                                                <option>Delaware</option>
+                                                <option>Tennessee</option>
+                                                <option>Texas</option>
+                                                <option>Washington</option>
+                                            </select>
                                         </div>
                                     </div>
                                     <!-- /.col -->
                                     <div class="col-md-6">
+                                        <!-- /.form-group -->
                                         <div class="form-group">
-                                            <label for="total_hadiah">Total Hadiah</label>
-                                            <input type="number" class="form-control" id="total_hadiah" name="total_hadiah" placeholder="Masukkan deskripsi">
+                                            <label>Disabled Result</label>
+                                            <select class="form-control select2" style="width: 100%;">
+                                                <option selected="selected">Alabama</option>
+                                                <option>Alaska</option>
+                                                <option>California</option>
+                                                <option>Delaware</option>
+                                                <option>Tennessee</option>
+                                                <option>Texas</option>
+                                                <option>Washington</option>
+                                            </select>
                                         </div>
+                                        <!-- /.form-group -->
                                     </div>
                                     <!-- /.col -->
                                 </div>
@@ -62,7 +67,7 @@
                         </div>
                         <div class="modal-footer justify-content-between">
                             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-primary">Save changes</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
                         </div>
                     </div>
                     <!-- /.modal-content -->
@@ -73,52 +78,67 @@
         </form>
 
         <!-- Modal Update -->
-        <?php foreach ($dataJuara as $data) : ?>
-            <form action="/daftar-juara/update/<?= $data->id_juara ?>" method="post">
-                <div class="modal fade" id="modal-lg-update<?= $data->id_juara ?>">
-                    <div class="modal-dialog modal-lg">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h4 class="modal-title">Tambah Daftar Sponsor</h4>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <input type="hidden" id="id" name="id">
-                                            <div class="form-group">
-                                                <label for="juara">Juara</label>
-                                                <input type="text" class="form-control" id="juara" name="juara" value="<?= $data->juara ?>">
-                                            </div>
-                                        </div>
-                                        <!-- /.col -->
-                                        <div class="col-md-6">
-                                            <div class="form-group">
-                                                <label for="total_hadiah">Total Hadiah</label>
-                                                <input type="number" class="form-control" id="total_hadiah" name="total_hadiah" value="<?= $data->total_hadiah ?>">
-                                            </div>
-                                        </div>
-                                        <!-- /.col -->
-                                    </div>
-                                    <!-- /.row -->
-                                </div>
-                                <!-- /.card-body -->
-                            </div>
-                            <div class="modal-footer justify-content-between">
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                <button type="submit" class="btn btn-primary">Save changes</button>
-                            </div>
+        <form>
+            <div class="modal fade" id="modal-lg-update">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title">Update Daftar Lomba</h4>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
                         </div>
-                        <!-- /.modal-content -->
+                        <div class="modal-body">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Minimal</label>
+                                            <select class="form-control select2" style="width: 100%;">
+                                                <option selected="selected">Alabama</option>
+                                                <option>Alaska</option>
+                                                <option>California</option>
+                                                <option>Delaware</option>
+                                                <option>Tennessee</option>
+                                                <option>Texas</option>
+                                                <option>Washington</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <!-- /.col -->
+                                    <div class="col-md-6">
+                                        <!-- /.form-group -->
+                                        <div class="form-group">
+                                            <label>Disabled Result</label>
+                                            <select class="form-control select2" style="width: 100%;">
+                                                <option selected="selected">Alabama</option>
+                                                <option>Alaska</option>
+                                                <option>California</option>
+                                                <option>Delaware</option>
+                                                <option>Tennessee</option>
+                                                <option>Texas</option>
+                                                <option>Washington</option>
+                                            </select>
+                                        </div>
+                                        <!-- /.form-group -->
+                                    </div>
+                                    <!-- /.col -->
+                                </div>
+                                <!-- /.row -->
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <div class="modal-footer justify-content-between">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
                     </div>
-                    <!-- /.modal-dialog -->
+                    <!-- /.modal-content -->
                 </div>
-                <!-- /.modal -->
-            </form>
-        <?php endforeach; ?>
+                <!-- /.modal-dialog -->
+            </div>
+            <!-- /.modal -->
+        </form>
 
         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-lg">
             Tambah Data
@@ -138,28 +158,41 @@
                         <tr>
                             <th style="width: 10px">No</th>
                             <th>Juara</th>
-                                <th>Total Hadiah</th>
-                                <th style="width: 200px">Aksi</th>
+                            <th>Total Hadiah</th>
+
+                            <th style="width: 200px">Aksi</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <?php foreach ($dataJuara as $data) : ?>
-                            <tr>
-                                <td>1.</td>
-                                <td><?= $data->juara ?></td>
-                                <td style="text-align: justify;">
-                                    <?= $data->total_hadiah ?>
-                                </td>
-                                <td>
+                        <tr>
+                            <td>1.</td>
+                            <td>Pemogramanan</td>
+                            <td style="text-align: justify;">
+                                Selamat datang di Lomba Pemrograman Poltek, ajang kompetisi bergengsi untuk para
+                                programmer dan developer dari seluruh Indonesia! Di sini, kreativitas, logika, dan
+                                keterampilan teknis Anda akan diuji melalui serangkaian tantangan pemrograman yang
+                                dirancang untuk mengasah kemampuan dan membuktikan siapa yang terbaik di bidangnya.
+                                Apa yang Bisa Anda Harapkan?
+                                Tantangan Beragam: Lomba ini terdiri dari berbagai kategori, termasuk pengembangan
+                                aplikasi web, mobile, keamanan siber, dan algoritma. Setiap tantangan dirancang untuk
+                                menguji kemampuan pemrograman Anda dalam berbagai aspek.
+                                Hadiah Menarik: Menangkan hadiah total puluhan juta rupiah, sertifikat, serta peluang
+                                eksklusif untuk bergabung dengan perusahaan teknologi terkemuka.
+                                Mentorship dan Workshop: Dapatkan bimbingan langsung dari para ahli industri dan ikuti
+                                berbagai workshop yang akan membantu Anda meningkatkan keterampilan pemrograman.
+                            </td>
 
-                                    <button type="button" class="btn btn-primary btn-sm" data-toggle="modal"
-                                        data-target="#modal-lg-update<?= $data->id_juara ?>">Update</button>
-                                    <button type="button" class="btn btn-danger btn-sm">Delete</button>
-                                </td>
-                            </tr>
-                            <!-- Tambahkan lebih banyak baris sesuai kebutuhan -->
-                            <?php endforeach; ?>
-   
+
+                            <td><span class="badge rounded-pill text-bg-primary bg-primary"
+                                    style="opacity: 50%;">Dibuka</span></td>
+                            <td>
+
+                                <button type="button" class="btn btn-primary btn-sm" data-toggle="modal"
+                                    data-target="#modal-lg-update">Update</button>
+                                <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                            </td>
+                        </tr>
+                        <!-- Tambahkan lebih banyak baris sesuai kebutuhan -->
                     </tbody>
                 </table>
             </div>
