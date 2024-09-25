5714ed03811f0cf01258c34ac27c6d728557efe4
Muhammad Khairin <88011174+mkhairin@users.noreply.github.com>
2024-09-25 12:36:41 +0800

    Add dists for user dashboard

---
+++ app/Views/user/dists/dashboarduser.php
@@ -0,0 +1,365 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <title>Dashboard User</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="assets/vendors/feather/feather.css" />
+  <link
+    rel="stylesheet"
+    href="assets/vendors/ti-icons/css/themify-icons.css" />
+  <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css" />
+  <link
+    rel="stylesheet"
+    href="assets/vendors/font-awesome/css/font-awesome.min.css" />
+  <link
+    rel="stylesheet"
+    href="assets/vendors/mdi/css/materialdesignicons.min.css" />
+  <!-- endinject -->
+  <!-- Plugin css-->
+  <!-- <link rel="stylesheet" href="assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css"> -->
+  <link
+    rel="stylesheet"
+    href="assets/vendors/datatables.net-bs5/dataTables.bootstrap5.css" />
+  <link
+    rel="stylesheet"
+    href="assets/vendors/ti-icons/css/themify-icons.css" />
+  <link
+    rel="stylesheet"
+    type="text/css"
+    href="assets/js/select.dataTables.min.css" />
+  <!-- End plugin css-->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="assets/css/style.css" />
+  <!-- endinject -->
+  <link rel="shortcut icon" href="assets/images/favicon.png" />
+</head>
+
+<body>
+  <div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+    <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+      <div
+        class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
+        <a class="navbar-brand brand-logo me-5" href="index.html">Ini Logo</a>
+        <a class="navbar-brand brand-logo-mini" href="index.html">Ini Logo</a>
+      </div>
+      <div
+        class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+        <button
+          class="navbar-toggler navbar-toggler align-self-center"
+          type="button"
+          data-toggle="minimize">
+          <span class="icon-menu"></span>
+        </button>
+        <ul class="navbar-nav mr-lg-2">
+          <li class="nav-item nav-search d-none d-lg-block">
+            <div class="input-group">
+              <div
+                class="input-group-prepend hover-cursor"
+                id="navbar-search-icon">
+                <span class="input-group-text" id="search">
+                  <i class="icon-search"></i>
+                </span>
+              </div>
+              <input
+                type="text"
+                class="form-control"
+                id="navbar-search-input"
+                placeholder="Search now"
+                aria-label="search"
+                aria-describedby="search" />
+            </div>
+          </li>
+        </ul>
+        <ul class="navbar-nav navbar-nav-right">
+          <li class="nav-item nav-settings d-none d-lg-flex">
+            <a class="nav-link" href="#">
+              <i class="icon-ellipsis"></i>
+            </a>
+          </li>
+        </ul>
+        <button
+          class="navbar-toggler navbar-toggler-right d-lg-none align-self-center"
+          type="button"
+          data-toggle="offcanvas">
+          <span class="icon-menu"></span>
+        </button>
+      </div>
+    </nav>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_sidebar.html -->
+      <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="dashboarduser.html">
+              <i class="icon-grid menu-icon"></i>
+              <span class="menu-title">Dashboard</span>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="informasilainnya.html">
+              <i class="icon-paper menu-icon"></i>
+              <span class="menu-title">Informasi Lainnya</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="row">
+            <div class="col-md-12 grid-margin">
+              <div class="row">
+                <div class="col-12 col-xl-8 mb-4 mb-xl-0">
+                  <h3 class="font-weight-bold">Hallo Teams!</h3>
+                  <h6 class="font-weight-normal mb-0">
+                    Semangat Dalam Mengerjakan Tugas Lomba Ini,
+                    <span class="text-primary">
+                      Tapi Jangan Terlalu Terbebani Ya!</span>
+                  </h6>
+                </div>
+                <div class="col-12 col-xl-4">
+                  <div class="justify-content-end d-flex">
+                    <div class="dropdown flex-md-grow-1 flex-xl-grow-0">
+                      <button
+                        class="btn btn-sm btn-light bg-white dropdown-toggle"
+                        type="button"
+                        id="dropdownMenuDate2"
+                        data-bs-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="true">
+                        <i class="mdi mdi-calendar"></i> Today 22 September
+                        2024
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <p class="card-title">Team Bahagia Mengoding :)</p>
+                  <p class="font-weight-500">
+                    Team Bahagia Mengoding telah mendaftar ke lomba kaltek
+                    pada divisi mikrotik, team ini memiliki anggota:
+                  </p>
+                  <p class="font-weight-500">1. Eren Yeager Sebagai Ketua</p>
+                  <p class="font-weight-500">
+                    2. Armin Artlet Sebagai Anggota
+                  </p>
+                  <p class="font-weight-500">
+                    3. Mikasa Ackerman Sebagai Anggota
+                  </p>
+                  <div class="d-flex flex-wrap">
+                    <div class="me-5 mt-3">
+                      <p class="text-muted">Di Bimbing Oleh</p>
+                      <h3 class="text-primary font-weight-medium">
+                        Levi Ackerman
+                      </h3>
+                    </div>
+                    <div class="me-5 mt-3">
+                      <p class="text-muted">Asal Sekolah</p>
+                      <h3 class="text-primary font-weight-medium">
+                        Pulau Paradise
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 grid-margin stretch-card">
+              <div class="card tale-bg">
+                <div class="card-people mt-auto">
+                  <!-- nanti diubah jadi gambar kategorinya misal kategori mikrotik jadi gambar mikrotik -->
+                  <img
+                    src="assets/images/dashboard/people.svg"
+                    alt="people" />
+                  <div class="weather-info">
+                    <div class="d-flex">
+                      <div>
+                        <h2 class="mb-0 font-weight-normal">
+                          <i class="mdi mdi-content-duplicate me-0"></i>
+                        </h2>
+                      </div>
+                      <div class="ms-2">
+                        <h4 class="location font-weight-normal">Kategori</h4>
+                        <h6 class="font-weight-normal">Mikrotik</h6>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-7 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <p class="card-title mb-2">Preview Tugas Kategori Mikrotik</p>
+                  <p class="mb-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Ab suscipit maxime perspiciatis. Neque beatae cumque
+                    consectetur corrupti, facere enim quidem aliquam numquam
+                    consequatur unde, mollitia velit cupiditate minus
+                    necessitatibus fugit.
+                  </p>
+                  <p class="mb-4">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Ab suscipit maxime perspiciatis. Neque beatae cumque
+                    consectetur corrupti, facere enim quidem aliquam numquam
+                    consequatur unde, mollitia velit cupiditate minus
+                    necessitatibus fugit.
+                  </p>
+                  <p class="card-title mb-2">Aturan Tugas</p>
+                  <ul class="mb-2">
+                    <li>Unduh Soal</li>
+                    <li>Dilarang Menggunakan AI Apapun</li>
+                    <li>Dilarang Menjoki ke pembimbing</li>
+                    <li>File harus dalam bentuk PDF lengkap dengan komen untuk menjelaskan detail yang diperlukan</li>
+                    <li>dan seterusnya</li>
+                  </ul>
+                  <div class="template-demo mt-2 d-flex justify-content-between align-items-center flex-wrap">
+                    <div class="buttons d-flex">
+                      <button type="button" class="btn btn-outline-primary btn-icon-text me-2">
+                        <i class="ti-download btn-icon-prepend"></i> Unduh Tugas
+                      </button>
+                      <button type="button" class="btn btn-primary btn-icon-text">
+                        <i class="ti-file btn-icon-prepend"></i> Submit Tugas
+                      </button>
+                    </div>
+                    <div class="deadline text-right">
+                      <p class="mb-2">Deadline</p>
+                      <h3 class="mb-0">12:00 WITA</h3>
+                      <p class="font-weight-500">14 Januari 2025</p>
+                    </div>
+                  </div>
+
+
+                </div>
+
+              </div>
+            </div>
+            <div class="col-md-5 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title mb-2">To Do Lists</h4>
+                  <p class="font-weight-500">Kami harap to do list simple ini dapat sedikit membantu dalam mengatur kegiatan dan tugas team anda</p>
+                  <div class="list-wrapper pt-2">
+                    <ul
+                      class="d-flex flex-column-reverse todo-list todo-list-custom">
+                      <li>
+                        <div class="form-check form-check-flat">
+                          <label class="form-check-label">
+                            <input class="checkbox" type="checkbox" /> Membuat flowchart kegiatan
+                          </label>
+                        </div>
+                        <i class="remove ti-close"></i>
+                      </li>
+                      <li class="completed">
+                        <div class="form-check form-check-flat">
+                          <label class="form-check-label">
+                            <input class="checkbox" type="checkbox" checked />
+                            latihan setting vlan pada mikrotik
+                          </label>
+                        </div>
+                        <i class="remove ti-close"></i>
+                      </li>
+                      <li>
+                        <div class="form-check form-check-flat">
+                          <label class="form-check-label">
+                            <input class="checkbox" type="checkbox" /> melakukan rapat dengan team
+                          </label>
+                        </div>
+                        <i class="remove ti-close"></i>
+                      </li>
+                      <li class="completed">
+                        <div class="form-check form-check-flat">
+                          <label class="form-check-label">
+                            <input class="checkbox" type="checkbox" checked />
+                            input tugas dan lainnya
+                          </label>
+                        </div>
+                        <i class="remove ti-close"></i>
+                      </li>
+                      <li>
+                        <div class="form-check form-check-flat">
+                          <label class="form-check-label">
+                            <input class="checkbox" type="checkbox" /> mencoba membuat form latihan
+                          </label>
+                        </div>
+                        <i class="remove ti-close"></i>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="add-items d-flex mb-0 mt-2">
+                    <input
+                      type="text"
+                      class="form-control todo-list-input"
+                      placeholder="Add new task" />
+                    <button
+                      class="add btn btn-icon text-primary todo-list-add-btn bg-transparent">
+                      <i class="icon-circle-plus"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
+        <footer class="footer">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">
+              Copyright © 2024.
+              <a href="#" target="_blank">HIMA TRKJ POLITALA</a> All rights reserved.
+            </span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">
+              Developed with dedication
+              <i class="ti-info-alt text-danger ms-1"></i>
+            </span>
+          </div>
+        </footer>
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+  <!-- plugins:js -->
+  <script src="assets/vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <script src="assets/vendors/chart.js/chart.umd.js"></script>
+  <script src="assets/vendors/datatables.net/jquery.dataTables.js"></script>
+  <!-- <script src="assets/vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script> -->
+  <script src="assets/vendors/datatables.net-bs5/dataTables.bootstrap5.js"></script>
+  <script src="assets/js/dataTables.select.min.js"></script>
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="assets/js/off-canvas.js"></script>
+  <script src="assets/js/template.js"></script>
+  <script src="assets/js/settings.js"></script>
+  <script src="assets/js/todolist.js"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="assets/js/jquery.cookie.js" type="text/javascript"></script>
+  <script src="assets/js/dashboard.js"></script>
+  <!-- <script src="assets/js/Chart.roundedBarCharts.js"></script> -->
+  <!-- End custom js for this page-->
+</body>
+
+</html>
