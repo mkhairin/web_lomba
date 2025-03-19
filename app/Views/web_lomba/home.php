43505746ae49fe0688ebd0e7a80c0193c77577d2
Muhammad Khairin <88011174+mkhairin@users.noreply.github.com>
2025-03-19 10:18:28 +0800

    Update home.php

--- app/Views/web_lomba/home.php
+++ app/Views/web_lomba/home.php
@@ -45,7 +45,7 @@
     <!-- Section Title -->
     <div class="container section-title" data-aos="fade-up">
       <h2>About Us</h2>
-      <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+      <!-- <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p> -->
     </div><!-- End Section Title -->
 
     <div class="container">
